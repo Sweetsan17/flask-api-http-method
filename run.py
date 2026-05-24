55032e04b7ba76a986d77c784547e76d069895bd
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-24 19:04:14 +0530

    flask project connected by mysql database

--- run.py
+++ run.py
@@ -2,3 +2,12 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 from datetime import datetime
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "mysql+pymysql://root:root123@localhost/practice_for_myself_db"
+)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
