0bfdb82256924d383206dc0feb100fc1e0b8ec6e
Sweetsan <coderintheworldhack100@gmail.com>
2026-05-24 19:19:13 +0530

    Error handling condition block  added

--- run.py
+++ run.py
@@ -5,9 +5,23 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+# Database Configurations
+
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     "mysql+pymysql://root:root123@localhost/practice_for_myself_db"
 )
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+# Error handling with if condition
+
+if __name__ == "__main__":
+    try:
+        with app.app_context():
+            db.session.execute(text("SELECT 1"))
+            print("SUCCESS: Your Database Have Connected Successfully >>>>")
+    except Exception as error:
+        print("ERROR: Your Database Connection Is Failed >>>>")
+        print(error)
+    app.run(debug=True, port=5200)
